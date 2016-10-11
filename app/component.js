3088c2439a6e6d26858f87763bb8cdb41b51d857
emyarod <allllaboutyou@gmail.com>
2016-10-11 00:53:38 -0700

    Test locally scoped CSS

--- app/component.js
+++ app/component.js
@@ -1,6 +1,9 @@
+import styles from './main.css';
+
 module.exports = () => {
   const element = document.createElement('h1');
   element.innerHTML = 'Hello world';
+  element.className = styles.pinkbg;
 
   return element;
 };
