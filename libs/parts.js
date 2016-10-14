63a63aeb8b9baa02a887e3d5f07ad1a5d10a2abf
emyarod <allllaboutyou@gmail.com>
2016-10-13 17:10:24 -0700

    Set environment variables

--- libs/parts.js
+++ libs/parts.js
@@ -55,3 +55,14 @@ exports.minify = () => ({
     }),
   ],
 });
+
+exports.setFreeVariable = (key, value) => {
+  const env = {};
+  env[key] = JSON.stringify(value);
+
+  return {
+    plugins: [
+      new webpack.DefinePlugin(env),
+    ],
+  };
+};
