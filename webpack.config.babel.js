9d95794ea7540599bbbcd698551e7ecf21f494ae
emyarod <allllaboutyou@gmail.com>
2016-10-13 18:22:44 -0700

    Add hashes to filenames

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -45,6 +45,12 @@ switch (process.env.npm_lifecycle_event) {
       common,
       {
         devtool: 'source-map',
+        output: {
+          path: PATHS.build,
+          filename: '[name].[chunkhash].js',
+          // This is used for require.ensure. The setup
+          // will work without but this is useful to set
+        },
       },
       parts.setFreeVariable(
         'process.env.NODE_ENV',
